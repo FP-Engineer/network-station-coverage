c6b97225116247c93bbc43eff314d4d9a99e24a2
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-09 21:53:03 +0200

    chore: configured linter

--- packages/network-monitor/.eslintrc.cjs
+++ packages/network-monitor/.eslintrc.cjs
@@ -1,27 +1,29 @@
-/* eslint-env node */
+const rules = {
+	ON: 2,
+	OFF: 0,
+	WARN: 1
+};
 
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-}
+	env: {
+		browser: true,
+		es2020: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: [
+		'react-refresh',
+	],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		'indent': [rules.ON, 'tab']
+	}
+};
