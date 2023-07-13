36f016f1572dea9e62b4103c8a75b714b463c07b
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-13 23:14:46 +0200

    chore: increased test coverage

--- packages/network-monitor/src/features/monitor/components/network/Network.spec.tsx
+++ packages/network-monitor/src/features/monitor/components/network/Network.spec.tsx
@@ -1,10 +1,30 @@
 import { render } from '@testing-library/react';
 import { Network } from './Network';
+import { Station } from '../../connectors';
 
 describe('Network Test Suite', () => {
 
-	it('renders without crashing.', () => {
+	it('renders without crashing fetching data.', () => {
+
+		render(<Network useStations={() => ({isLoading: true})} />);
+	});
+
+	it('renders without crashing without data.', () => {
 
 		render(<Network useStations={() => ({isLoading: false})} />);
+	});
+
+	it('renders without crashing with data.', () => {
+		const stations: Station[] = [{
+			location: { x: 0, y: 0 },
+			reach: 0,
+		}];
+
+		render(<Network useStations={() => ({isLoading: false, data: stations})} />);
+	});
+
+	it('renders without crashing with connector error.', () => {
+
+		render(<Network useStations={() => ({isLoading: false, error: 'Lorem Ipsum.'})} />);
 	});
 });
