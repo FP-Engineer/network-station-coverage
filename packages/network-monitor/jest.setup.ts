c402a1dc67a5971d0a51606f15a0c6169dade343
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-15 11:58:07 +0200

    chore: prepared test setup to work with vite envs

--- packages/network-monitor/jest.setup.ts
+++ packages/network-monitor/jest.setup.ts
@@ -1,57 +1,10 @@
 import 'cross-fetch/polyfill';
-import ServiceMock from 'openapi-backend';
-import type { Document } from 'openapi-backend';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
+import server from './mocks/server';
 
-import json from 'api-schemas/src/station-api.json';
+jest.mock('./src/app/Constants.ts', () => ({
+	networkDataServiceUrl: '',
+}));
 
-const stationsApi = new ServiceMock({ definition: json as Document });
-stationsApi.register('notFound', (_, res, ctx) => res(ctx.status(404)));
-stationsApi.register('notImplemented', async (c, res, ctx) => {
-	const { status, mock } = stationsApi.mockResponseForOperation(c.operation.operationId ?? 'no id');
-	ctx.status(status);
-	return res(ctx.json(mock));
-});
-stationsApi.register('validationFail', (c, res, ctx) => res(
-	ctx.status(400),
-	ctx.json({ error: c.validation.errors }),
-));
-
-const server = setupServer(
-	rest.get('/success_json', (_, res, ctx) => res(
-		ctx.status(200),
-		ctx.json({
-			payload: 'success',
-		}),
-	)),
-	rest.get('/success_not_json', (_, res, ctx) => res(
-		ctx.status(200),
-		ctx.text('not a json'),
-	)),
-	rest.get('/failure', (_, res, ctx) => res(
-		ctx.status(418),
-	)),
-	rest.post('/success', (_, res, ctx) => res(
-		ctx.status(200),
-		ctx.json({ message: 'received' }),
-	)),
-	rest.post('/failure', (_, res, ctx) => res(
-		ctx.status(400),
-		ctx.json({ code: 1000, message: 'Bad Request' }),
-	)),
-	rest.get('/stations', async (req, res, ctx) => stationsApi.handleRequest(
-		{
-			path: req.url.pathname,
-			query: req.url.search,
-			method: req.method,
-			body: req.bodyUsed ? await req.json() : null,
-			headers: { ...req.headers.raw },
-		},
-		res,
-		ctx,
-	)),
-);
-  
 beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
---
+++ packages/network-monitor/mocks/handlers.ts
@@ -0,0 +1,52 @@
+import ServiceMock from 'openapi-backend';
+import type { Document } from 'openapi-backend';
+import { DefaultBodyType, MockedRequest, RestHandler, rest } from 'msw';
+
+import json from 'api-schemas/src/station-api.json';
+
+const stationsApi = new ServiceMock({ definition: json as Document });
+stationsApi.register('notFound', (_, res, ctx) => res(ctx.status(404)));
+stationsApi.register('notImplemented', async (c, res, ctx) => {
+	const { status, mock } = stationsApi.mockResponseForOperation(c.operation.operationId ?? 'no id');
+	ctx.status(status);
+	return res(ctx.json(mock));
+});
+stationsApi.register('validationFail', (c, res, ctx) => res(
+	ctx.status(400),
+	ctx.json({ error: c.validation.errors }),
+));
+
+export default [
+	rest.get('/success_json', (_, res, ctx) => res(
+		ctx.status(200),
+		ctx.json({
+			payload: 'success',
+		}),
+	)),
+	rest.get('/success_not_json', (_, res, ctx) => res(
+		ctx.status(200),
+		ctx.text('not a json'),
+	)),
+	rest.get('/failure', (_, res, ctx) => res(
+		ctx.status(418),
+	)),
+	rest.post('/success', (_, res, ctx) => res(
+		ctx.status(200),
+		ctx.json({ message: 'received' }),
+	)),
+	rest.post('/failure', (_, res, ctx) => res(
+		ctx.status(400),
+		ctx.json({ code: 1000, message: 'Bad Request' }),
+	)),
+	rest.get('/stations', async (req, res, ctx) => stationsApi.handleRequest(
+		{
+			path: req.url.pathname,
+			query: req.url.search,
+			method: req.method,
+			body: req.bodyUsed ? await req.json() : null,
+			headers: { ...req.headers.raw },
+		},
+		res,
+		ctx,
+	))
+] as RestHandler<MockedRequest<DefaultBodyType>>[];
