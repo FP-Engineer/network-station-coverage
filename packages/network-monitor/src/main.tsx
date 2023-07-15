b5fab186ab7ee541241947adca96be9e70e25f3c
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-15 23:09:47 +0200

    chore: implemented dev server

--- packages/network-monitor/src/main.tsx
+++ packages/network-monitor/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App.tsx';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+if (import.meta.env.MODE === 'mock') {
+	const { default: worker } = await import('../mocks/browser.ts');
+	worker.start();
+}
+
+root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
-)
+);
