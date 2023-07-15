d8fc365b364531818269668b72cf9f7d1a203c27
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-15 23:20:16 +0200

    chore: refactored vite config

--- packages/network-monitor/vite.config.ts
+++ packages/network-monitor/vite.config.ts
@@ -3,55 +3,39 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react-swc';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
-export default defineConfig({
-	plugins: [
+export default ({ mode }) => {
+
+	const plugins = [
 		react(),
 		vanillaExtractPlugin(),
-		nodePolyfills({
-			exclude: [
-				'fs', // Excludes the polyfill for `fs` and `node:fs`.
-			],
-			globals: {
-				Buffer: true, // can also be 'build', 'dev', or false
-				global: true,
-				process: true,
-			},
-			// Whether to polyfill `node:` protocol imports.
-			protocolImports: true,
-		})
-	],
-	envDir: './environments',
-	define: {
-		'__dirname': JSON.stringify(__dirname)
+	];
+	const options = {};
+
+	if (mode === 'mock') {
+		plugins.push(
+			nodePolyfills({
+				exclude: [
+					'fs',
+				],
+				globals: {
+					Buffer: true,
+					global: true,
+					process: true,
+				},
+				protocolImports: true,
+			})
+		);
+
+		Object.assign(options, {
+			define: {
+				'__dirname': JSON.stringify(__dirname)
+			}
+		});
 	}
-})
 
-//export default ({ mode }) => {
-
-//	const plugins = [
-//		react(),
-//		vanillaExtractPlugin()
-//	];
-
-//	if (mode === 'mock') {
-//		plugins.push(nodePolyfills({
-//			// To exclude specific polyfills, add them to this list.
-//			exclude: [
-//				'fs', // Excludes the polyfill for `fs` and `node:fs`.
-//			],
-//			// Whether to polyfill specific globals.
-//			globals: {
-//				Buffer: true, // can also be 'build', 'dev', or false
-//				global: true,
-//				process: true,
-//			},
-//			// Whether to polyfill `node:` protocol imports.
-//			protocolImports: true,
-//		}));
-//	}
-
-//	return defineConfig({
-//		envDir: './environments',
-//		plugins,
-//	});
-//};
+	return defineConfig({
+		plugins,
+		envDir: './environments',
+		...options,
+	});
+}
