ea64caa1cccd5d55351ba9a2b84c07c0f006831f
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-21 13:14:03 +0200

    feat: visualized unconnected devices

--- packages/network-monitor/src/features/monitor/components/device/Device.tsx
+++ packages/network-monitor/src/features/monitor/components/device/Device.tsx
@@ -1,13 +1,16 @@
+import cn from 'classnames';
 import { faMobile } from '@fortawesome/free-solid-svg-icons';
 import { Item } from '../item';
 import { DeviceViewModel } from '../../models/device';
+import { container, notConnected } from './Device.css'; 
 
 export type Props = DeviceViewModel;
 
 export const Device: React.FC<Props> = ({ location, coveredByStationAt, withSpeed }) => {
 
-	const deviceLocationLabel = `location: (${ location.x }, ${ location.y })`
-	const coverageByStationAtLabel = coveredByStationAt
+	const deviceLocationLabel = `location: (${ location.x }, ${ location.y })`;
+	const isConnected = !!coveredByStationAt;
+	const coverageByStationAtLabel = isConnected
 		? `covered by station at: (${coveredByStationAt.x}, ${coveredByStationAt.y})`
 		: 'not connected';
 	const withSpeedLabel = coveredByStationAt
@@ -20,9 +23,11 @@ export const Device: React.FC<Props> = ({ location, coveredByStationAt, withSpee
 	].filter((x): x is string => !!x);
 
 	return (
-		<Item icon={ faMobile }>
-			{ labels.map((x) => <>{ x }</>) }
-		</Item>
+		<div className={cn(container, { [notConnected]: !isConnected}) }>
+			<Item icon={ faMobile }>
+				{ labels.map((x) => <>{ x }</>) }
+			</Item>
+		</div>
 	);
 };
 
