b06d970823c900b2d49e8e781381449b774985cc
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-21 13:14:03 +0200

    feat: visualized unconnected devices

--- packages/network-monitor/src/features/monitor/components/device/Device.css.ts
+++ packages/network-monitor/src/features/monitor/components/device/Device.css.ts
@@ -3,3 +3,15 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
 	display: 'inline-block',
 });
+
+export const notConnected = style({
+	position: 'relative',
+	'::after': {
+		position: 'absolute',
+		pointerEvents: 'none',
+		content: 'x',
+		color: 'red',
+		right: '0',
+		bottom: '0',
+	}
+});
